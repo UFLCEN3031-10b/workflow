25baffa0b2ddeae6a34795afc4129e501010b011
John Kaufmann <kaufmann43@gmail.com>
2015-09-11 15:55:36 +0000

    finished product

--- script.js
+++ script.js
@@ -3,14 +3,28 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
-  $scope.newItem = "";
+  $scope.todos = [{
+    "priority": "now",
+    "text": "Learn Angular",
+  }, {
+    "priority": "now",
+    "text": "Learn node"
+  }];
+  $scope.newItem = {
+    "priority": "",
+    "text": ""
+  };
   
-  $scope.addItem = function(){
+  $scope.addItem = function(event){
+    var itemCopy = {};
     console.log("in add");
-    if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
-      $scope.newItem = "";
+    console.log($scope.newItem);
+    if ($scope.newItem.text !== ""){
+      itemCopy.priority = $scope.newItem.priority;    
+      itemCopy.text = $scope.newItem.text;
+      $scope.todos.push(itemCopy);
+      $scope.newItem.text = "";
+      $scope.newItem.priority = "";
     }
   }
     
@@ -19,13 +33,18 @@ myApp.controller('MainCtrl', function ($scope){
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
   }
-    
+  
   
 });
 
 /*************************
  * Homework (not rly):
  * - "enter" button functionality instead of clicking button
+ * 
+ *      html - new button with input that the JS can read from
+ *      edit function (in JS) happens when an edit button is clicked
+ *      selects the object from the array, updates its values, and replaces it in the array
+ *      
  * - edit button functionality
  * - button to mark item as "complete"
  * - have a total number of items at the top
